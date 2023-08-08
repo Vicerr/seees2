899a1d4b8a161a28b3f7c2b427508a6cd90adbfb
Victor Odoi <115374614+Vicerr@users.noreply.github.com>
2023-08-08 03:20:15 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 // change background image of hero section dynamically in html
 if(document.getElementById('imageOne')){
 let imageOne = document.getElementById('imageOne')
@@ -149,7 +151,7 @@ $(document).ready(function (){
     const options = {
         root: null,
         rootMargin: '0px',
-        threshold: 0
+        threshold: 0.35,
     }
 
     //fade items in using observer
@@ -179,6 +181,73 @@ $(document).ready(function (){
 
     // cookies
     
+//     function hasAcceptedCookies(){
+//         return document.cookie.includes("cookiesAccepted=true")
+//     }
+//     function hasRejectedCookies(){
+//         return document.cookie.includes("cookiesRejected=true")
+//     }
+//     function setCookie(name, value, days){
+//         const date = new Date();
+//         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+//         const expires = "expires=" + date.toUTCString();
+//         document.cookie = name + "=" + value + ";" + expires + ";path=/";
+//     }
+
+//     //if cookies has not been accepted,
+//     function showCookiesPopup(){
+//         const cookiesPopup = $('#cookiesPopup')
+
+//         //check if user has accepted cookies
+//         if(!hasAcceptedCookies()){
+//             // cookiesPopup.addClass("show")
+//             cookiesPopup.css("opacity", 0)
+//                 cookiesPopup.delay(5000).animate({"opacity":1}, 500);
+            
+//         }else{
+//             cookiesPopup.hide()
+//         }
+//     }
+
+//     function createCookiesPopup() {
+//         const cookiesPopup = $("<div>", {
+//             class: "cookies-popup",
+//             id: "cookiesPopup",
+//             html: `
+//                <div class="container-fluid">
+//              <h3>Cookies <i class="fa-solid fa-cookie cookie-icon"></i></h3>             
+//                 <p> This website uses cookies to improve your experience. By continuing to use this site, you must have read our <a href="/cookiepolicy"> Cookies Policy</a>.</p>
+//                <div class="cookie-button-container"> <button id="acceptCookiesButton">Accept</button>
+//                <button id="rejectCookiesButton">Reject</button></div>
+//                </div>
+//             `
+//         })
+//         $('body').append(cookiesPopup)
+//     }
+//     function hideCookiesPopup(accepted){
+//         const cookiesPopup = $('#cookiesPopup')
+
+//         if(accepted){
+//             setCookie("cookiesAccepted", "true", 30)
+//         }else{
+//             setCookie("cookiesRejected", "true", 5)
+//         }
+//         $(cookiesPopup).fadeOut();
+//     }
+
+//     createCookiesPopup()
+//     $("#acceptCookiesButton").on("click", function(){
+//         hideCookiesPopup(true)
+//     })
+//     $("#rejectCookiesButton").on("click", function(){
+//         hideCookiesPopup(false)
+//     })
+//     showCookiesPopup()
+
+$(document).ready(function (){
+
+    // cookies
+    
     function hasAcceptedCookies(){
         return document.cookie.includes("cookiesAccepted=true")
     }
@@ -197,7 +266,7 @@ $(document).ready(function (){
         const cookiesPopup = $('#cookiesPopup')
 
         //check if user has accepted cookies
-        if(!hasAcceptedCookies()){
+        if(!hasAcceptedCookies() && !hasRejectedCookies()){
             // cookiesPopup.addClass("show")
             cookiesPopup.css("opacity", 0)
                 cookiesPopup.delay(5000).animate({"opacity":1}, 500);
@@ -212,10 +281,12 @@ $(document).ready(function (){
             class: "cookies-popup",
             id: "cookiesPopup",
             html: `
-               <div class="container"> <p> This website uses cookies to improve your experience. By continuing to use this site, you must have read our <a href="/privacypolicy"> Privacy Policy</a>.</p>
-               <div class="cookie-button-container"> <button id="acceptCookiesButton">Accept</button>
-               <button id="rejectCookiesButton">Reject</button></div>
-               </div>
+            <div class="container-fluid">
+                          <h3>Cookies <i class="fa-solid fa-cookie cookie-icon"></i></h3>             
+                             <p> This website uses cookies to improve your experience. By continuing to use this site, you must have read our <a href="/cookiepolicy"> Cookies Policy</a>.</p>
+                            <div class="cookie-button-container"> <button id="acceptCookiesButton">Accept</button>
+                            <button id="rejectCookiesButton">Reject</button></div>
+                          </div>
             `
         })
         $('body').append(cookiesPopup)
@@ -239,6 +310,7 @@ $(document).ready(function (){
         hideCookiesPopup(false)
     })
     showCookiesPopup()
+})
 
 })
 
